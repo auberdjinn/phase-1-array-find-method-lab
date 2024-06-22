c1932eba569799da56b17e0f9682e3895711f91a
auberdjinn <auberdjinn@proton.me>
2024-06-21 20:17:55 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+
+function superbowlWin(arr) {
+    let val = arr.find(function (val) {
+        return val.result === "W";
+    });
+
+    if (val === undefined) {
+        return undefined;
+    }
+    return val.year;
+}
